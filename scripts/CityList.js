aa32c06d58347c254a949d39ac7ff1a4174ee6ee
Punam Ahire <punambahire@gmail.com>
2022-03-01 14:56:06 -0600

    resolved all bugs and added indentation using css

---
+++ scripts/CityList.js
@@ -0,0 +1,16 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ul>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li>${walker.city}</li>`
+    }
+
+    citiesHTML += "</ul>"
+
+    return citiesHTML
+}
